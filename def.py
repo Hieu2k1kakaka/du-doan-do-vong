780b27b7563df49d7980a3194eaf41c6365a03b6
Hieu2k1kakaka <103618896+Hieu2k1kakaka@users.noreply.github.com>
2025-05-16 15:16:59 +0700

    Update def.py

--- def.py
+++ def.py
@@ -64,7 +64,8 @@ if st.button("Dự đoán độ võng cực đại"):
 
     # Vẽ mô hình cây dầm Cantilever
     x = np.linspace(0, L, 100)
-    y = -(F * x**2) / (6 * E * (b * h**3)) * (3 * L - x)  # Công thức dầm cantilever
+    I = (b * h**3) / 12
+    y = -(F * x**2) / (6 * E * I) * (3 * L - x) # Công thức dầm cantilever
 
     fig, ax = plt.subplots(figsize=(8, 3))
     ax.plot(x, y, color='blue', linewidth=3)
